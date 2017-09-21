4a1d839613655e48b7c5fa2d5cfc3a7faa8c4f72
DANIEL LOPEZ ROMAN <dlopezro15.alumnes@ub052229.mat.ub.edu>
2017-09-21 17:09:15 +0200

    fgets actualizado

--- P1/src/fgets.c
+++ P1/src/fgets.c
@@ -1,22 +1,45 @@
 #include <stdio.h>
 
+char* slice_str(const char * str, size_t end)
+{
+    const size_t len = strlen(str);
+    char buffer[len + 1];
+    size_t j = 0;
+    for ( size_t i = 0; i <= end; ++i ) {
+        buffer[j++] = str[i];
+    }
+    buffer[j] = 0;
+    return buffer;
+}
+
+
 int main()
 {
    FILE *fp;
+   int longFile;
    char str[100];
-
+   char **vector;
+   
    /* opening file for reading */
-   fp = fopen("file.txt" , "r");
+   fp = fopen("../dades/strings.txt" , "r");
    if(fp == NULL) 
    {
       perror("Error opening file");
       return(-1);
    }
-   if( fgets (str, 100, fp)!=NULL ) 
+   //cuantas lineas hay
+   longFile = fgets(str, 100, fp)
+   vector = (char *) malloc(longFile*sizeof(char[100]);
+   for(longFile; longFile>1; longFile--)
    {
-      /* writing content to stdout */
-      puts(str);
+      fgets(str, 100, fp);
+      //guardar menos el ultimo caracter (es un salto de linea)
+      vector[longFile-1] = slice_str(str,strlen(str)-1);
    }
+   //este es el ultimo, no tiene salto de linea
+   fgets(str, 100, fp)
+   vector[0] = str;
+   
    fclose(fp);
    
    return(0);
