95892fc6d2f88d026e224d4428942274d23020b6
DANIEL LOPEZ ROMAN <dlopezro15.alumnes@ub052229.mat.ub.edu>
2017-09-28 18:35:43 +0200

    Cambio en cuicsort cmp, lee dinamicamente, lo pide practica

---
+++ P1/src/quicksort_cmp_lectura_eficiente_dinamica.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int compara(const void *p1, const void *p2)
+{
+  char *str1, *str2;
+
+  str1 = *((char **) p1); 
+  str2 = *((char **) p2);
+
+  return strcmp(str1,str2);
+}
+
+char* slice_str(const char * str, size_t end)
+{
+    const size_t len = strlen(str);
+    char *buffer;
+    buffer = (char*)malloc((len+1)*sizeof(char));
+    
+    size_t j = 0;
+    for ( size_t i = 0; i <= end; ++i ) {
+        buffer[j++] = str[i];
+    }
+    buffer[j] = 0;
+    return buffer;
+}
+
+
+int main()
+{
+   FILE *fp;
+   int longFile;
+   char str[100];
+   char **vector;
+   int i;   
+
+   /* opening file for reading */
+   fp = fopen("../dades/strings.txt" , "r");
+   if(fp == NULL) 
+   {
+      perror("Error opening file");
+      return(-1);
+   }
+   //cuantas lineas hay
+   longFile = atoi(fgets(str, 100, fp));
+   vector = (char **) malloc(longFile*sizeof(char*));
+   if(vector==NULL){
+	return 1;
+   }
+   for(i=0; i<longFile; i++)
+   {
+      fgets(str, 100, fp);
+      
+      //guardar menos el ultimo caracter (es un salto de linea)
+      str = slice_str(str,strlen(str)-2);
+      vector[i] = (char *) malloc(sizeof(char)*strlen(str))
+      vector[i] = str
+   }
+
+   fclose(fp);
+   
+   qsort(vector, longFile, sizeof(char*), compara);
+   
+   for(i=0; i<longFile;i+=100)printf("%s\n",vector[i]);
+   
+   return(0);
+}
